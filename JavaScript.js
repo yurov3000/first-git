76a6fe8a4fd65207aef2364bc6c79c7910494eb3
Danil Yurov <yurov3000@gmail.com>
2020-03-17 19:38:52 +0700

    first commit

---
+++ JavaScript.js
@@ -0,0 +1,6 @@
+var answer = confirm("Вам есть 18 лет?");
+// var answer = prompt("Вам есть 18 лет?", "ДА.")
+console.log(answer);
+console.log(typeof(answer));
+
+console.log(answer +' Если да, отлично!');
